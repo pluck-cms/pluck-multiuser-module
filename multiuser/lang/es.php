7b66529b2ea28934837ec32afb109dba3a0a6a40
ekyo-pl <ekyo.serwis@gmail.com>
2015-10-29 10:37:11 +0100

    by Filisko
    by Filisko

---
+++ multiuser/lang/es.php
@@ -0,0 +1,17 @@
+<?php
+$lang['multiuser']['title'] = 'Multiusuario';
+$lang['multiuser']['delete_user'] = 'Eliminar usuario';
+$lang['multiuser']['new_user'] = 'Nuevo usuario';
+$lang['multiuser']['edit_user'] = 'Editar usuario';
+$lang['multiuser']['new'] = 'Anadir nuevo usuario';
+$lang['multiuser']['list'] = 'Lista de usuarios:';
+$lang['multiuser']['login'] = 'Usuario';
+$lang['multiuser']['pass'] = 'Contrasena';
+$lang['multiuser']['role'] = 'Permisos';
+$lang['multiuser']['no_login'] = 'Introduzca el usuario';
+$lang['multiuser']['login_exists'] = 'Acceso bloqueado';
+$lang['multiuser']['no_pass'] = 'Introduzca la contrasena';
+$lang['multiuser']['error'] = 'Es imposible autoinstalar el módulo de multiusuarios. Tienes que copiar manualmente multiuser\lib\multiuser.php a tu directorio raíz.';
+$lang['multiuser']['intro'] = 'Módulo multiusuario y multiadmin';
+$lang['multiuser']['no_permisions'] = 'Sin permisos';
+?>
